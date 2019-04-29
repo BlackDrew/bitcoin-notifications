8453c12d6c9ac4e1356021f200416e692eda75b5
Tobi Dahunsi <dahunsitobi5@gmail.com>
2019-04-29 18:15:50 +0100

    new project

---
+++ bitcoin_notification.py
@@ -0,0 +1,24 @@
+from requests import Request, Session
+from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
+import json
+
+url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+parameters = {
+  'start':'1',
+  'limit':'1',
+  'convert':'NGN'
+}
+headers = {
+  'Accepts': 'application/json',
+  'X-CMC_PRO_API_KEY': '93b28ac3-6df1-4ef3-8fd5-2cabc03d79e8',
+}
+
+session = Session()
+session.headers.update(headers)
+
+try:
+  response = session.get(url, params=parameters)
+  data = json.loads(response.text)
+  print(data)
+except (ConnectionError, Timeout, TooManyRedirects) as e:
+  print(e)
